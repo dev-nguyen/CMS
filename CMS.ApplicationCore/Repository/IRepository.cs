ea67280e63309e86dd28201bad9e1f3b6160ce85
dev-nguyen <thanhnguyenbk@gmail.com>
2021-08-04 11:51:59 +0700

    Init

---
+++ CMS.ApplicationCore/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.ApplicationCore.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        public IQueryable<T> GetItems();
+        public IQueryable<T> GetItems(Expression<Func<T, bool>> condition);
+        public T GetItem(object Id);
+
+        public void AddItem(T Entity);
+        public void AddItems(IEnumerable<T> entities);
+    }
+}
